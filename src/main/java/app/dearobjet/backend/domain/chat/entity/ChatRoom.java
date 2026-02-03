12e16e4aa3a81d81ebaa53a7f7796db98063480d
GYEONGNAM <feels19@naver.com>
2026-02-03 21:13:19 +0900

    Revert "[FEAT] 회원 기능 구현"

--- src/main/java/app/dearobjet/backend/domain/chat/entity/ChatRoom.java
+++ src/main/java/app/dearobjet/backend/domain/chat/entity/ChatRoom.java
@@ -4,8 +4,6 @@ import app.dearobjet.backend.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "chat_room")
 @Getter
@@ -17,17 +15,14 @@ public class ChatRoom extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "chat_room_id")
-    private Long id;
+    private Long chatRoomId;
 
     @Column(name = "last_modified_at")
-    private LocalDateTime lastModifiedAt;
+    private java.time.LocalDateTime lastModifiedAt;
+
+    @Column(name = "unread_count")
+    private Integer unreadCount;
 
     @Column(name = "last_message")
     private String lastMessage;
-
-    // 방의 상태를 갱신하는 비즈니스 메서드
-    public void updateLastMessage(String message, LocalDateTime now) {
-        this.lastMessage = message;
-        this.lastModifiedAt = now; // 방의 정렬 순서를 위해 시간 갱신
-    }
 }
