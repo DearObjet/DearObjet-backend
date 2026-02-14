2208a223b357600d9d274f79d3a782834a48466d
ruudska6 <feels19@naver.com>
2026-02-14 14:41:31 +0900

    delete: User-> Customer로 역할 변경으로 인한 Role 필드 제거

--- src/main/java/app/dearobjet/backend/domain/user/dto/CustomerSignupRequest.java
+++ src/main/java/app/dearobjet/backend/domain/user/dto/CustomerSignupRequest.java
@@ -1,17 +1,15 @@
 package app.dearobjet.backend.domain.user.dto;
 
-import app.dearobjet.backend.domain.user.enums.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CompleteSignupRequest {
+public class CustomerSignupRequest {
 
     private String name;
     private String email;
     private String phoneNumber;
     private Boolean smsAgreement;
     private Boolean marketingAgreement;
-    private Role role; // CUSTOMER / ARTIST / SHOP
 }
