44bdf48b1981bcc978eedc40ee28ea0989cc2b62
ruudska6 <feels19@naver.com>
2026-02-21 19:25:13 +0900

    feat: Swagger 의존성 및 어노테이션 추가

--- src/main/java/app/dearobjet/backend/domain/user/controller/UserController.java
+++ src/main/java/app/dearobjet/backend/domain/user/controller/UserController.java
@@ -5,6 +5,9 @@ import app.dearobjet.backend.domain.user.dto.UserSignupRequest;
 import app.dearobjet.backend.domain.user.service.UserService;
 import app.dearobjet.backend.global.api.ApiResponse;
 import app.dearobjet.backend.global.auth.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +17,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "User", description = "사용자 가입 API")
 public class UserController {
 
     private final UserService userService;
 
     // CUSTOMER 가입 완료
     @PostMapping("/complete")
+    @Operation(summary = "일반 사용자 가입")
     public ResponseEntity<ApiResponse<Void>> completeCustomerSignup(
+            @Parameter(hidden = true)
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody UserSignupRequest request
     ) {
@@ -34,7 +40,9 @@ public class UserController {
 
     // ARTIST 가입 완료
     @PostMapping("/complete/artist")
+    @Operation(summary = "작가 사용자 가입")
     public ResponseEntity<ApiResponse<Void>> completeArtistSignup(
+            @Parameter(hidden = true)
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody BusinessSignupRequest request
     ) {
@@ -48,7 +56,9 @@ public class UserController {
 
     // SHOP 가입 완료
     @PostMapping("/complete/shop")
+    @Operation(summary = "상점 사용자 가입")
     public ResponseEntity<ApiResponse<Void>> completeShopSignup(
+            @Parameter(hidden = true)
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody BusinessSignupRequest request
     ) {
