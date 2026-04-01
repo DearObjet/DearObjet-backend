f7384b7ea245c89eab684c7ecc93190e04648489
ruudska6 <feels19@naver.com>
2026-04-01 21:11:12 +0900

    feat: 클래스 등록 수정 삭제 구현

---
+++ src/main/java/app/dearobjet/backend/domain/classes/dto/UpdateClassRequest.java
@@ -0,0 +1,29 @@
+package app.dearobjet.backend.domain.classes.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateClassRequest {
+
+    @NotBlank
+    private String className;
+
+    @NotBlank
+    private String classDescription;
+
+    @NotNull
+    @DecimalMin("0.0")
+    private Double price;
+
+    @NotNull
+    @Min(1)
+    private Integer maxCapacity;
+
+    private String notes;
+}
