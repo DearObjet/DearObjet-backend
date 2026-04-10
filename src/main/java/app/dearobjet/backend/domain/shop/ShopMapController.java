7ea47aa252d2a3c4be440ff2796b4384a318db48
ruudska6 <feels19@naver.com>
2026-04-10 14:29:19 +0900

    feat: 위도 경도를 계산하는 배치 API 추가

--- src/main/java/app/dearobjet/backend/domain/shop/ShopMapController.java
+++ src/main/java/app/dearobjet/backend/domain/shop/ShopMapController.java
@@ -1,10 +1,12 @@
 package app.dearobjet.backend.domain.shop;
 
+import app.dearobjet.backend.domain.shop.dto.ShopGeocodeResponse;
 import app.dearobjet.backend.domain.shop.dto.ShopMapResponse;
 import app.dearobjet.backend.domain.shop.service.ShopMapService;
 import app.dearobjet.backend.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class ShopMapController {
     @GetMapping
     public ApiResponse<ShopMapResponse> getShopMarkers() {
         return ApiResponse.of(shopMapService.getShopMarkers());
+    }
+
+    @PostMapping("/geocode")
+    public ApiResponse<ShopGeocodeResponse> geocodeShops() {
+        return ApiResponse.of(shopMapService.geocodeShops());
     }
 }
