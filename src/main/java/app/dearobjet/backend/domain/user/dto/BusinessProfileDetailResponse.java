b32d8d9f693cd9b4fd9e9a43d178eb8c47008624
ruudska6 <feels19@naver.com>
2026-04-17 20:11:28 +0900

    refactor: 내려줄 것이 많은 객체여서 Record -> Builder 패턴으로 변경

--- src/main/java/app/dearobjet/backend/domain/user/dto/BusinessProfileDetailResponse.java
+++ src/main/java/app/dearobjet/backend/domain/user/dto/BusinessProfileDetailResponse.java
@@ -2,45 +2,61 @@ package app.dearobjet.backend.domain.user.dto;
 
 import app.dearobjet.backend.domain.user.entity.BusinessProfile;
 import app.dearobjet.backend.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record BusinessProfileDetailResponse(
-        String profileUrl,
-        String phoneNumber,
-        String email,
-        String instagramId,
-        String businessName,
-        String businessNumber,
-        String ownerName,
-        String businessAddress,
-        String bankName,
-        String bankAccountNumber,
-        String accountHolder,
-        String bankbookImageUrl,
-        Boolean isBankbookVerified,
-        String taxInvoiceEmail,
-        String hometaxApiKey
-) {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BusinessProfileDetailResponse {
+
+    private String profileUrl;
+    private String userName;
+    private String phoneNumber;
+    private String email;
+    private String businessPhoneNumber;
+    private String instagramId;
+
+    private String businessName;
+    private String businessNumber;
+    private String ownerName;
+    private String businessAddress;
+
+    private String bankName;
+    private String bankAccountNumber;
+    private String accountHolder;
+    private String bankbookImageUrl;
+    private Boolean isBankbookVerified;
+
+    private String taxInvoiceEmail;
+    private String hometaxApiKey;
+
     public static BusinessProfileDetailResponse from(
             User user,
             BusinessProfile businessProfile,
             String instagramId
     ) {
-        return new BusinessProfileDetailResponse(
-                user.getProfileUrl(),
-                user.getPhoneNumber(),
-                user.getEmail(),
-                instagramId,
-                businessProfile.getBusinessName(),
-                businessProfile.getBusinessNumber(),
-                businessProfile.getOwnerName(),
-                businessProfile.getBusinessAddress(),
-                businessProfile.getBankName(),
-                businessProfile.getBankAccountNumber(),
-                businessProfile.getAccountHolder(),
-                businessProfile.getBankbookImageUrl(),
-                businessProfile.getIsBankbookVerified(),
-                businessProfile.getTaxInvoiceEmail(),
-                businessProfile.getHometaxApiKey()
-        );
+        return BusinessProfileDetailResponse.builder()
+                .profileUrl(user.getProfileUrl())
+                .userName(user.getName())
+                .phoneNumber(user.getPhoneNumber())
+                .email(user.getEmail())
+                .businessPhoneNumber(businessProfile.getBusinessPhoneNumber())
+                .instagramId(instagramId)
+                .businessName(businessProfile.getBusinessName())
+                .businessNumber(businessProfile.getBusinessNumber())
+                .ownerName(businessProfile.getOwnerName())
+                .businessAddress(businessProfile.getBusinessAddress())
+                .bankName(businessProfile.getBankName())
+                .bankAccountNumber(businessProfile.getBankAccountNumber())
+                .accountHolder(businessProfile.getAccountHolder())
+                .bankbookImageUrl(businessProfile.getBankbookImageUrl())
+                .isBankbookVerified(businessProfile.getIsBankbookVerified())
+                .taxInvoiceEmail(businessProfile.getTaxInvoiceEmail())
+                .hometaxApiKey(businessProfile.getHometaxApiKey())
+                .build();
     }
 }
